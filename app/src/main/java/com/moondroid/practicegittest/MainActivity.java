092d5317f27070c2a491ce7f084b7ea3b1e66fb8
byldh124 <byldh124@gmail.com>
2021-02-05 15:16:36 +0900

    Initial commit

---
+++ app/src/main/java/com/moondroid/practicegittest/MainActivity.java
@@ -0,0 +1,14 @@
+package com.moondroid.practicegittest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
